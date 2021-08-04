7f9f98edbbef1975d046bb9acfc7e05a000a1bac
Daniel Silva <dani3l.dxp@gmail.com>
2021-08-04 16:26:42 -0300

    feat: buscando os dados com SSG e tipando os dados

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,24 @@
-export default function Home() {
+import { GetStaticProps } from "next";
+
+import { Episode } from "../types/types";
+
+type HomeProps = {
+  episodes: Array<Episode>;
+};
+
+export default function Home({ episodes }: HomeProps) {
+  console.log(episodes);
   return <h1>Index</h1>;
 }
+
+export const getStaticProps: GetStaticProps = async () => {
+  const response = await fetch("http://localhost:3333/episodes");
+  const data = await response.json();
+
+  return {
+    props: {
+      episodes: data,
+    },
+    revalidate: 60 * 60 * 8, // 8 hours
+  };
+};
