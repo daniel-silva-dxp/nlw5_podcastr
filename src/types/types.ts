a1d52d3b1133bb5d18e47b14ff4dd41f0b34d983
Daniel Silva <dani3l.dxp@gmail.com>
2021-08-05 09:50:07 -0300

    feat: editando o type Episode

--- src/types/types.ts
+++ src/types/types.ts
@@ -5,4 +5,9 @@ export type Episode = {
   description: string;
   published_at: string;
   thumbnail: string;
+  file: {
+    duration: number;
+    durationAsString: string;
+    url: string;
+  };
 };
