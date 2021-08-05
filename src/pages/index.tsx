8fb87d6a7c4f9dbc16fe1174960c410a14c43031
Daniel Silva <dani3l.dxp@gmail.com>
2021-08-05 09:51:31 -0300

    feat: formatando os dados de Episode e passando via props

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,10 @@
 import { GetStaticProps } from "next";
+import { format, parseISO } from "date-fns";
+import ptBR from "date-fns/locale/pt-BR";
 
 import { api } from "../services/api";
 import { Episode } from "../types/types";
+import { convertDurationToTimeString } from "../utils/convertDurationToTimeString";
 
 type HomeProps = {
   episodes: Episode[];
@@ -21,9 +24,29 @@ export const getStaticProps: GetStaticProps = async () => {
     },
   });
 
+  const episodes = data.map((episode: Episode) => {
+    return {
+      id: episode.id,
+      title: episode.title,
+      members: episode.members,
+      description: episode.description,
+      thumbnail: episode.thumbnail,
+      published_at: format(parseISO(episode.published_at), "dd MMM yy", {
+        locale: ptBR,
+      }),
+      file: {
+        duration: Number(episode.file.duration),
+        durationAsString: convertDurationToTimeString(
+          Number(episode.file.duration)
+        ),
+        url: episode.file.url,
+      },
+    };
+  });
+
   return {
     props: {
-      episodes: data,
+      episodes,
     },
     revalidate: 60 * 60 * 8, // 8 hours
   };
